6a0d56b40b4507db7c3df7ab50da8a395ed7547f
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-12-21 19:32:32 +0100

    letre kell hozni a kepviselo entitast

---
+++ src/main/java/com/aron/voting/dao/model/Kepviselo.java
@@ -0,0 +1,43 @@
+package com.aron.voting.dao.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Kepviselo {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "kepviselo_sequence")
+    @SequenceGenerator(name = "kepviselo_sequence", sequenceName = "kepviselo_sequence", allocationSize = 1)
+    private Long id;
+
+    @Column(name = "kepviseloKod",  unique = true)
+    private String kepviseloKod;
+
+    @PrePersist
+    public void generateVotingId() {
+        this.kepviseloKod = "Kepviselo" + this.id;
+    }
+
+    @OneToMany(mappedBy = "elnok", cascade = CascadeType.MERGE)
+    private Set<Szavazas> szavazasSet = new HashSet<>();
+
+
+    @OneToMany(mappedBy = "kepviselo")
+    private Set<Szavazatok> szavazatok = new HashSet<>();
+
+
+
+
+}
