0a19aa2b117e677e3dfe73ebd54216e727e77e33
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-12-21 19:38:44 +0100

    letre kell hozni a Szavazatok entitast ami tarolja az egyes kepviselo szavazatat

---
+++ src/main/java/com/aron/voting/dao/model/Szavazatok.java
@@ -0,0 +1,35 @@
+package com.aron.voting.dao.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Szavazatok {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Szavazatok(Kepviselo kepviselo, Szavazat szavazat) {
+        this.kepviselo = kepviselo;
+        this.szavazat = szavazat;
+    }
+
+    @ManyToMany(mappedBy = "szavazatok", cascade = CascadeType.MERGE)
+    private Set<Szavazas> szavazasSet = new HashSet<>();
+
+    @ManyToOne
+    @JoinColumn(name = "kepviselo_id")
+    private Kepviselo kepviselo;
+
+    @Enumerated(EnumType.STRING)
+    private Szavazat szavazat;
+}
