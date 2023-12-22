5b0714c47296bee2b8e0a701891821cf53f3fabb
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-12-22 21:34:06 +0100

    validálni kell, ha null vagy üres érékeket kapok

--- src/main/java/com/aron/voting/dto/UjSzavazasDTO.java
+++ src/main/java/com/aron/voting/dto/UjSzavazasDTO.java
@@ -1,6 +1,9 @@
 package com.aron.voting.dto;
 
 import com.aron.voting.dao.model.Tipus;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
@@ -9,9 +12,9 @@ import java.util.Set;
 
 public record UjSzavazasDTO(
         @NotNull LocalDateTime idopont,
-        @NotNull String targy,
+        @NotBlank String targy,
         @NotNull Tipus tipus,
-        @NotNull String elnok,
-        @NotNull Set<SzavazatDTO> szavazatok
+        @NotBlank String elnok,
+        @Valid Set<@NotNull SzavazatDTO> szavazatok
 ) {
 }
