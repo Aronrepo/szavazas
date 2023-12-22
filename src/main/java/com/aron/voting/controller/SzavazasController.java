d9e17d4aa3fd231d322d5cae3a6b2360a9724ec4
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-12-22 21:32:51 +0100

    módosítani mentésnél a választ

--- src/main/java/com/aron/voting/controller/SzavazasController.java
+++ src/main/java/com/aron/voting/controller/SzavazasController.java
@@ -3,10 +3,13 @@ package com.aron.voting.controller;
 import com.aron.voting.dto.SzavazasValaszDTO;
 import com.aron.voting.dto.UjSzavazasDTO;
 import com.aron.voting.service.SzavazasService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/szavazasok")
@@ -20,8 +23,10 @@ public class SzavazasController {
     }
 
     @PostMapping("/szavazas")
-    public ResponseEntity<SzavazasValaszDTO> hozzaadUjSzavazast(@RequestBody UjSzavazasDTO ujSzavazas) {
-        SzavazasValaszDTO szavazasValaszDTO = new SzavazasValaszDTO(szavazasService.ujSzavazas(ujSzavazas));
+    public ResponseEntity<?> hozzaadUjSzavazast(@Valid @RequestBody UjSzavazasDTO ujSzavazas) {
+
+        String szavazasId = szavazasService.ujSzavazas(ujSzavazas);
+        SzavazasValaszDTO szavazasValaszDTO = new SzavazasValaszDTO(Map.of("szavazasId", szavazasId));
         return new ResponseEntity<>(szavazasValaszDTO, HttpStatus.CREATED);
     }
 }
