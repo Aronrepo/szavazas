8a585f341802d8ef89d58669c9d3211d705de4c3
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-12-23 13:14:02 +0100

    le kell kezelni, ha több szavazás érkezik be egy képviselőtől és időpont sem egyezhet

--- src/main/java/com/aron/voting/service/SzavazasService.java
+++ src/main/java/com/aron/voting/service/SzavazasService.java
@@ -7,12 +7,15 @@ import com.aron.voting.dao.model.Szavazatok;
 import com.aron.voting.dto.SzavazasValaszDTO;
 import com.aron.voting.dto.UjSzavazasDTO;
 import com.aron.voting.exception.ElnoknekNincsSzavazataException;
+import com.aron.voting.exception.SzavazasAzonosIdobenException;
+import com.aron.voting.exception.TobbMintEgySzavazatException;
 import com.aron.voting.repositories.SzavazasRepository;
 import com.aron.voting.repositories.SzavazatokRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -61,8 +64,34 @@ public class SzavazasService {
             throw new ElnoknekNincsSzavazataException("Az elnöknek nincs szavazata");
         }
 
+        boolean egyszerreCsakEgySzavazatLehet = kepviseloknekNincsKettoSzavazata(szavazatok);
+
+        if(!egyszerreCsakEgySzavazatLehet) {
+            throw new TobbMintEgySzavazatException("Egy képviselő csak egyszer szavazhat");
+        }
+
+
+        Szavazas szavazasEbbenAzIdoponban = szavazasRepository.findSzavazasByIdopont(szavazas.getIdopont());
+
+        if(szavazasEbbenAzIdoponban != null) {
+            throw new SzavazasAzonosIdobenException("Ugyanarra az időpontra nem lehet két szavazást felvinni");
+        }
+
         Szavazas savedSzavazas = szavazasRepository.save(szavazas);
 
         return savedSzavazas.getSzavazas_id();
+    }
+
+    private boolean kepviseloknekNincsKettoSzavazata(Set<Szavazatok> osszesSzavazat) {
+        Set<Kepviselo> talaltKepviselok = new HashSet<>();
+
+        for (Szavazatok szavazatok : osszesSzavazat) {
+            if(!talaltKepviselok.add(szavazatok.getKepviselo())) {
+                return false;
+            }
+
+        }
+        return true;
+
     }
 }
