907d79177a46160bf8b228ff0508ac9f1542a65c
Aron Tas <66522767+Aronrepo@users.noreply.github.com>
2023-12-23 13:13:33 +0100

    le kell kezelni, ha több szavazás érkezik be egy képviselőtől

---
+++ src/main/java/com/aron/voting/exception/TobbMintEgySzavazatException.java
@@ -0,0 +1,7 @@
+package com.aron.voting.exception;
+
+public class TobbMintEgySzavazatException extends RuntimeException {
+    public TobbMintEgySzavazatException(String message) {
+        super(message);
+    }
+}
